82c4554d146f467f624f1243a94a3592ea9f2017
KHashimoto3 <hashi.koki385@gmail.com>
2024-01-02 17:11:11 +0900

    サンプルのポートフォリオリストを表示できるようにした。

--- front/src/app/_home-sections/HomePortfolios.tsx
+++ front/src/app/_home-sections/HomePortfolios.tsx
@@ -1,15 +1,51 @@
-import { Container } from "@mui/material";
+"use client";
+
+import { Container, Grid } from "@mui/material";
 import PortfolioCard from "../_portfolio-components/portfolioCard";
+import { PortfolioList } from "../types/portfolioList";
 
 export default function HomePortfolios() {
+  const samplePortfilioList: PortfolioList[] = [
+    {
+      id: 1,
+      title: "制作物1",
+      description: "制作物の説明1",
+      imgPath: "/sample_pc.png",
+    },
+    {
+      id: 2,
+      title: "制作物2",
+      description: "制作物の説明2",
+      imgPath: "/sample_pc.png",
+    },
+    {
+      id: 3,
+      title: "制作物3",
+      description: "制作物の説明3",
+      imgPath: "/sample_pc.png",
+    },
+    {
+      id: 4,
+      title: "制作物4",
+      description: "制作物の説明4",
+      imgPath: "/sample_pc.png",
+    },
+  ];
+
   return (
     <Container maxWidth="md">
       <h2 style={{ textAlign: "center" }}>~Portfolios~</h2>
-      <PortfolioCard
-        title="制作物1"
-        description="制作物の説明1"
-        imgPath="/sample_pc.png"
-      />
+      <Grid container spacing={2}>
+        {samplePortfilioList.map((portfolio) => (
+          <Grid item xs={4} key={portfolio.id}>
+            <PortfolioCard
+              title={portfolio.title}
+              description={portfolio.description}
+              imgPath={portfolio.imgPath}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </Container>
   );
 }
