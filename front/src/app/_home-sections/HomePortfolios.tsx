d81535b5a4621eb56c3cefd9fa416cf9a414e06b
KHashimoto3 <hashi.koki385@gmail.com>
2024-01-02 17:02:45 +0900

    ポートフォリオカードのコンポーネントにpropsを追加した。

--- front/src/app/_home-sections/HomePortfolios.tsx
+++ front/src/app/_home-sections/HomePortfolios.tsx
@@ -5,7 +5,11 @@ export default function HomePortfolios() {
   return (
     <Container maxWidth="md">
       <h2 style={{ textAlign: "center" }}>~Portfolios~</h2>
-      <PortfolioCard />
+      <PortfolioCard
+        title="制作物1"
+        description="制作物の説明1"
+        imgPath="/sample_pc.png"
+      />
     </Container>
   );
 }
--- front/src/app/_portfolio-components/portfolioCard.tsx
+++ front/src/app/_portfolio-components/portfolioCard.tsx
@@ -9,7 +9,15 @@ import {
 } from "@mui/material";
 import PortfolioDetailButton from "../_ui/button/portfolioDetailButton";
 
-export default function PortfolioCard() {
+type Props = {
+  title: string;
+  description: string;
+  imgPath: string;
+};
+
+export default function PortfolioCard(props: Props) {
+  const { title, description, imgPath } = props;
+
   const onClickFunc = () => {
     alert("ポートフォリオ詳細ボタンが押されました。");
   };
@@ -19,15 +27,15 @@ export default function PortfolioCard() {
       <CardMedia
         component="img"
         height="140"
-        image="/sample_pc.png"
+        image={imgPath}
         alt="portfolio image"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          カードのタイトル
+          {title}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          カードの説明文
+          {description}
         </Typography>
       </CardContent>
       <CardActions>
