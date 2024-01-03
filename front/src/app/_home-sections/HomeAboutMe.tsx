bb976394a2647847a9aad32708daa52fee20c1f9
KHashimoto3 <hashi.koki385@gmail.com>
2024-01-03 11:10:54 +0900

    About meの画像を設定した。

--- front/src/app/_home-sections/HomeAboutMe.tsx
+++ front/src/app/_home-sections/HomeAboutMe.tsx
@@ -1,10 +1,33 @@
-import { Container } from "@mui/material";
+"use client";
+
+import { Box, Container, Grid } from "@mui/material";
 
 export default function HomeAboutMe() {
   return (
     <Container maxWidth="md">
       <h2 style={{ textAlign: "center" }}>~About me~</h2>
-      <p>自己紹介がきます。</p>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
+          >
+            <img
+              src="/about_me_icon.jpg"
+              alt="about_me_icon"
+              style={{ maxWidth: "300px" }}
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <p>自己紹介がきます。</p>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
