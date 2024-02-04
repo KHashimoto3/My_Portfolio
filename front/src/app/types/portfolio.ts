1dc93d701bf995b4581a71ab43293539fb68d474
KHashimoto3 <hashi.koki385@gmail.com>
2024-02-04 10:39:50 +0900

    portfolioのデータ形式を定義した。

---
+++ front/src/app/types/portfolio.ts
@@ -0,0 +1,14 @@
+export type Portfolio = {
+  id: number;
+  title: string;
+  imgPath: string;
+  abstract: string;
+  background: string;
+  functions: string;
+  thoughts: string[];
+  stacks: string[];
+  period: string;
+  position: string;
+  link: string;
+  githubLink: string;
+};
